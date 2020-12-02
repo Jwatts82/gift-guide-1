da783ba033f5f326d9ed76ad14d4434052832099
jwatts82 <jessperr182@hotmail.com>
2020-12-01 22:44:02 -0800

    update index actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
 
     def welcome
     end
+
+    def authentication 
+        if !user_signed_in? 
+          redirect_to '/'
+        end 
+    end 
     
 end
--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,15 +1,22 @@
 class GiftsController < ApplicationController
+    before_action :authentication
 
     def index
         @gifts = current_user.gifts
     end
 
     def new
-
+        @gift = current_user.gifts.new
     end
 
     def create
-
+        @gift = current_user.gifts.build(gift_params)
+        if @gift.valid?
+            @gift.save
+            redirect_to @gift
+        else
+            render :new
+        end
     end
 
     def show
@@ -26,6 +33,21 @@ class GiftsController < ApplicationController
 
     def delete
 
+    end
+
+    private
+    #strong params with keys, has many 
+    def gift_params
+        params.require(:gift).permit(
+            :name,
+            :comment,
+            :store,
+            :price,
+            :list_id,
+            list_attributes: [
+                :name
+            ]
+        )
     end
 
 end
