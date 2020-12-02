a8934b9e60710350da069c5537623e1a66b2dda1
jwatts82 <jessperr182@hotmail.com>
2020-12-01 21:00:55 -0800

    initial routes set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
+  
   root to: 'application#welcome'
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new', as: 'login'
     get 'signup', to: 'devise/registrations#new', as: 'signup' 
   end
+
+  resources :lists do
+    resources :gifts 
+
+  resources :gifts
+  resources :lists
 end
