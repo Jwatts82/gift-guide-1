09e63e48917465631c7376a60df781e5c9eae37c
jwatts82 <jessperr182@hotmail.com>
2020-12-01 21:31:35 -0800

    setup controller defs

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,2 +1,31 @@
 class GiftsController < ApplicationController
+
+    def index
+        @gifts = current_user.gifts
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+    
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def delete
+
+    end
+
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,11 @@
 class ListsController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+
+    end
+    
 end
