5be98fef3937e1c7955c4e0dba9ccaebaa95d983
jwatts82 <jessperr182@hotmail.com>
2020-12-02 00:37:16 -0800

    add index and show to list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,24 @@
 class ListsController < ApplicationController
 
     def index
-
+        if user_signed_in?
+            @lists = current_user.lists.uniq
+        else
+            redirect_to '/'
+        end
     end
 
     def show
+        if user_signed_in?
+            @list = current_user.lists.find_by(id: params[:id])
+            if @list
+                @gifts = current_user.gifts.where(list_id: params[:id])
+                render :show
+            end
+
+        else
+            redirect_to '/'
+        end
 
     end
     
